fabf479c04f56c8ee948616fef2b33cb209d8e77
lsm <sm00216@naver.com>
2024-07-29 22:35:43 +0900

    mealkit project

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,81 +1,117 @@
 import React from 'react'
+import './Header.css'
 import { FiSearch } from "react-icons/fi";
+import { FaRegCircleXmark } from "react-icons/fa6";
+import { MdOutlineContactPage } from "react-icons/md";
+import { FaBasketShopping } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <header className='relative w-full'>
-        <div>
-          <h1 className='absolute uppercase text-5xl top-11 left-80 text-orange-500 font-bold'>
+    <header className='header'>
+      <div className='header-in'>
+        <h1 className='logo'>
             <a href="/">mealkit</a>
-          </h1> 
-          {/* 페이지 로고 */}
-          <div className='absolute top-2.5 right-96'>
-            <ul className='list-none flex'>
-              <li className='text-sm text-gray-500'>
+        </h1> 
+        {/* 페이지 로고 */}
+        <div className='join'>
+            <ul>
+              <li>
                 <a href="#">회원가입</a>
               </li>
-              <li className='px-3 text-xs text-gray-300'>|</li>
-              <li className='text-sm text-gray-500'>
+              <li>
                 <a href="#">로그인</a>
               </li>
             </ul>
-          </div>
-          {/* 회원가입&로그인 */}
-          <div className='absolute top-12 left-[700px] text-center'>
-            <div className='relative inline-block w-[510px] h-[50px] outline outline-[3px] outline-orange-500 rounded-3xl bg-white pr-[45px] pl-5'>
+        </div>
+        {/* 회원가입&로그인 */}
+        <div className='search'>
+          <div className='search-in'>
               <input type='search' 
                     placeholder='상품을 검색하세요.'
-                    className='w-full h-[50px] border-0 p-0 text-sm text-[#555] text-left bg-transparent outline-none align-middle'
+                    className='search-input'
               />
               <button type='button' 
-                    className='absolute  top-3 right-3'
+                    className='search-btn'
               >
                 <FiSearch size={26} style={{ color : 'FF6B00' }}/>
               </button>
+          </div>
+          {/* search 창 */}
+          <div className='search-layer'>
+            <div className='popular'>
+                <h2>인기 검색어</h2>
             </div>
-            {/* search 창 */}
-            <div className='absolute top-[54px] left-0 w-full min-h-[100px] bg-white border-solid border-[1px] border-[#666] rounded-2xl'>
-              <div className='px-5 pt-[30px] pb-2.5 border-solid border-b-[1px] border-[#666]'>
-                <h2 className='text-[#222] text-[15px] text-left font-bold'>인기 검색어</h2>
-              </div>
-              <div className='px-2.5 overflow-hidden'>
-                <ol className='relative list-none float-left w-full'>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>1 대파 육개장</a>
+            <div className='popular-rank'>
+                <ol className='list'>
+                  <li>
+                    <a href="#">1 대파 육개장</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>2 갈릭 부채살 스테이크</a>
+                  <li>
+                    <a href="#">2 갈릭 부채살 스테이크</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>3 황태콩나물국밥</a>
+                  <li>
+                    <a href="#">3 황태콩나물국밥</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>4 베이컨쉬림프 알리오올리오</a>
+                  <li>
+                    <a href="#">4 베이컨쉬림프 알리오올리오</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>5 짬뽕</a>
+                  <li>
+                    <a href="#">5 짬뽕</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>6. 라뽁이</a>
+                  <li>
+                    <a href="#">6. 라뽁이</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>7 김치찌개</a>
+                  <li>
+                    <a href="#">7 김치찌개</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>8 된장찌개</a>
+                  <li>
+                    <a href="#">8 된장찌개</a>
                   </li>
-                  <li className='text-left border-solid border-b-[1px] border-[#e6e6e6] h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>9 파스타</a>
+                  <li>
+                    <a href="#">9 파스타</a>
                   </li>
-                  <li className='text-left h-[50px]'>
-                    <a href="#" className='block text-[#666] py-3.5 pl-[7px] pr-[35px]'>10 부대찌개</a>
+                  <li>
+                    <a href="#">10 부대찌개</a>
                   </li>
                 </ol>
-              </div>
             </div>
             {/* 인기&최근 검색어창(click->open) */}
+            <div className='recent-search'>
+                <div className='recent'>
+                  <h3>최근검색어</h3>
+                  <button className='search-delete'>
+                    <span>전체삭제</span>
+                  </button>
+                </div>
+                <div className='recent-layer'>
+                  {/* 검색어 있는 경우 */}
+                  {/* 검색어 없는 경우 */}
+                  <div className='no-data'>
+                    <FaRegCircleXmark className='x-mark'/>
+                    최근 검색어가 없습니다.
+                  </div>
+                </div>
+            </div>
+            {/* 최근검색어 */}
           </div>
         </div>
+        {/* 검색레이어 */}
+        <div className='my-menu'>
+          <ul>
+            <li>
+              <a href='#'>
+                <a>
+                  <MdOutlineContactPage className='my-page-icon'/>
+                </a>
+              </a>
+            </li>
+            <li>
+              <a href='#'><FaBasketShopping className='basket-icon'/></a>
+            </li>
+          </ul>
+        </div>
+        {/* 마이메뉴 */}
+      </div>
     </header>
   )
 }
